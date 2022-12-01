cef6463e6a07859e16ee5faf103bb65359a4c5c1
Oleg Chumin <tschumin.oleg@gmail.com>
2022-12-01 14:07:49 +0300

    Change game files

---
+++ src/main/java/MusicPlayerWAV/WAV_Player.java
@@ -0,0 +1,45 @@
+package MusicPlayerWAV;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class WAV_Player {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        File file = new File("src/main/java/MusicPlayerWAV/09 Money for Nothing.wav");
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+
+        String response = "";
+        Scanner scanner = new Scanner(System.in);
+
+        // P - play, S - Stop, R - reset, Q - quit
+
+        while (!response.equals("Q")) {
+            System.out.println("P - play, S - Stop, R - reset, Q - quit");
+            System.out.println("Enter your choice: ");
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch (response) {
+                case ("P"):
+                    clip.start();
+                    break;
+                case ("S"):
+                    clip.stop();
+                    break;
+                case ("R"):
+                    clip.setMicrosecondPosition(0);
+                    break;
+                case ("Q"):
+                    break;
+                default:
+                    System.out.println("Not a valid response!");
+                    break;
+            }
+        }
+        System.out.println("Byyeeee!");
+    }
+}
